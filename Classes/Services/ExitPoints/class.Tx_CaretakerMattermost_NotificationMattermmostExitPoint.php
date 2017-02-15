cf8dc0fef648c56a9e33bd317483422ac1cfadbd
Nicole Cordes <typo3@cordes.co>
2017-02-15 22:26:52 +0100

    [BUGFIX] Fix execute() function

--- Classes/Services/ExitPoints/class.Tx_CaretakerMattermost_NotificationMattermmostExitPoint.php
+++ Classes/Services/ExitPoints/class.Tx_CaretakerMattermost_NotificationMattermmostExitPoint.php
@@ -113,7 +113,7 @@ class Tx_CaretakerMattermost_NotificationMattermmostExitPoint extends tx_caretak
      */
     public function execute()
     {
-        if (!empty($this->aggregatedNotifications)) {
+        if (!empty($this->config['aggregateNotifications'])) {
             foreach ($this->aggregatedNotifications as $channel => $instanceNotifications) {
                 ksort($instanceNotifications);
                 foreach ($instanceNotifications as $aggregatedNotifications) {

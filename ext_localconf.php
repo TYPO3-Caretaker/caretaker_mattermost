59089eeaeec8b31eee85ba3f93730fa926fbec33
Nicole Cordes <typo3@cordes.co>
2018-08-16 16:29:40 +0200

    [BUGFIX] Remove phar file in favour of composer installation

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,10 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
+if (!class_exists('ThibaudDauce\\Mattermost\\Mattermost')) {
+    require \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('caretaker_mattermost') . 'Resources/Php/vendor/autoload.php';
+}
+
 tx_caretaker_ServiceHelper::registerNotificationExitPoint(
     'caretaker_mattermost',
     'Classes/Services/ExitPoints',

af25c295793a4d832bee84eca9d9ea372ce4c917
Jan Haffner <jan.haffner@dkd.de>
2020-05-12 18:43:56 +0200

    [TASK] Add support for TYPO3 v8

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '7.6.0-7.6.99',
+      'typo3' => '7.6.0-8.7.99',
       'caretaker' => '1.0.0-1.99.99',
     ),
     'conflicts' => 
